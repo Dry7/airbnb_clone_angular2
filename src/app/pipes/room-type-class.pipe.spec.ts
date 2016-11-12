d9c30f2bac66708ecc0032de5049726511cfde29
Dry7 <andrey.dry7.shilov@yandex.ru>
2016-11-12 23:21:40 +0300

    Room type Pipe

---
+++ src/app/pipes/room-type-class.pipe.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { RoomTypeClassPipe } from './room-type-class.pipe';
+
+describe('Pipe: RoomTypeClass', () => {
+  it('create an instance', () => {
+    let pipe = new RoomTypeClassPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
